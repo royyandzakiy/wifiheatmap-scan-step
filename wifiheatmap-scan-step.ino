83674ae922523d16d56cd0b2ff7eec30ab329738
royyandzakiy <royyan.dzakiy@efishery.com>
2021-04-23 17:49:39 +0700

    feat:removed servo and changed to using stepper for vertical movement

--- wifiheatmap-scan-step.ino
+++ wifiheatmap-scan-step.ino
@@ -20,71 +20,96 @@
 // STEPPER
 // Controls stepper, giving horizontal rotations
 
-#define STEPS_MAX 1000
-#define STEPS_PER_REVOLUTION 200  // change this to fit the number of steps per revolution for your motor
-int step_count_max;               // maximum step movement allowed to get a 360 degree turn. will be initiated in setup_stepper()
-int step_count = 0;               // number of steps the motor has taken
+#define STEPS_A_REVOLUTION 200  // change this to fit the number of steps per revolution for your motor
 
 // initialize the stepper library on pins 8 through 11:
-#define PIN_IN1 25
-#define PIN_IN2 33
-#define PIN_IN3 32
-#define PIN_IN4 35
-Stepper my_stepper(STEPS_PER_REVOLUTION, PIN_IN1, PIN_IN2, PIN_IN3, PIN_IN4);
+#define STPV_STEP_PIN 5   // GPIO5 = D1
+#define STPV_DIR_PIN 4    // GPIO4 = D2
+#define STPH_STEP_PIN 0   // GPIO0 = D3
+#define STPH_DIR_PIN 2    // GPIO2 = D4
+
+#define STEP_DELAY_MS 500
+
+int vrtStep = 2;
+int hrzStep = 20;
+
+int vrtStepPos = 0;
+int hrzStepPos = 0;
 
 void setup_stepper() {
-  pinMode(PIN_IN1, OUTPUT);
-  pinMode(PIN_IN2, OUTPUT);
-  pinMode(PIN_IN3, OUTPUT);
-  pinMode(PIN_IN4, OUTPUT);
+  // setup stepper vertical
+  pinMode(STPV_STEP_PIN, OUTPUT);
+  pinMode(STPV_DIR_PIN, OUTPUT);
 
-  my_stepper.setSpeed(20);
-  
-  step_count_max = STEPS_MAX / STEPS_PER_REVOLUTION;
+  // setup stepper horizontal
+  pinMode(STPH_STEP_PIN, OUTPUT);
+  pinMode(STPH_DIR_PIN, OUTPUT);
 }
 
-void step() {
-  // step one step:
-  my_stepper.step(1);
-  ++step_count;
-  Serial.print("steps:");
-  Serial.println(step_count);
+void step_dir_cw(int pin, bool cw) {
+  if (cw) {
+    digitalWrite(pin,HIGH);
+  } else {
+    digitalWrite(pin,LOW);
+  }
+}
+
+void step(int pin, int steps) {
+  // Makes 200 pulses for making one full cycle rotation
+  for(int x = 0; x < steps; x++) {
+    digitalWrite(pin,HIGH); 
+    delayMicroseconds(STEP_DELAY_MS); 
+    digitalWrite(pin,LOW); 
+    delayMicroseconds(STEP_DELAY_MS); 
+  }
+}
+
+void vertical_step(int steps) {
+  // move vertically for strafing
+  step(STPV_STEP_PIN, steps);
+  vrtStepPos += steps;
+}
+
+void horizontal_step(int steps) {
+  // move horizontally  for panning
+  step(STPH_STEP_PIN, steps);
+  hrzStepPos += steps;
 }
 
 //=====================================
-// SERVO
+// SERVO ---> NOT USED ANYMORE
 // Controls servo, giving vertical rotations
 
-#define SERVO_PIN 2           // Declare the Servo pin  2 = D4
-#define SERVO_ANGLE_INC 10    // how much degree is added on one servo move
-#define SERVO_ANGLE_MAX 90
-
-Servo my_servo; // Create a servo object 
-bool turn_on = false;
-uint16_t servo_angle = 0;
-bool to_increase = true;
-
-void setup_servo() { 
-   my_servo.attach(SERVO_PIN); 
-}
-
-bool servo_move() { 
-  // the servo moves in a zigzag manner, it will move up to down, then down to up
-  my_servo.write(servo_angle);
-
-  int servo_angle_change = (to_increase?1:-1) *  SERVO_ANGLE_INC;
-  servo_angle = servo_angle + servo_angle_change; 
-  
-  if (servo_angle > SERVO_ANGLE_MAX) {
-    to_increase = false;
-    return true; // reached edge
-  } else if (servo_angle < 0) {
-    to_increase = true;
-    return true; // reached edge
-  } else {
-    return false;
-  }
-}
+//#define SERVO_PIN 4           // Declare the Servo pin  2 = D4
+//#define SERVO_ANGLE_INC 10    // how much degree is added on one servo move
+//#define SERVO_ANGLE_MAX 90
+//
+//Servo my_servo; // Create a servo object 
+//bool turn_on = false;
+//uint16_t servo_angle = 0;
+//bool to_increase = true;
+//
+//void setup_servo() { 
+//   my_servo.attach(SERVO_PIN); 
+//}
+//
+//bool servo_move() { 
+//  // the servo moves in a zigzag manner, it will move up to down, then down to up
+//  my_servo.write(servo_angle);
+//
+//  int servo_angle_change = (to_increase?1:-1) *  SERVO_ANGLE_INC;
+//  servo_angle = servo_angle + servo_angle_change; 
+//  
+//  if (servo_angle > SERVO_ANGLE_MAX) {
+//    to_increase = false;
+//    return true; // reached edge
+//  } else if (servo_angle < 0) {
+//    to_increase = true;
+//    return true; // reached edge
+//  } else {
+//    return false;
+//  }
+//}
 
 //=======================================
 // WIFI HEATMAP
@@ -113,7 +138,6 @@ void setup_wifi() {
 
 float wifi_meanrssi()
 {
-
   // WiFi.scanNetworks will return the number of networks found
   int n = WiFi.scanNetworks();
   #ifdef SCAN_RESULT_PRINT
@@ -158,7 +182,6 @@ void setup() {
   Serial.begin(115200);
   Serial.println("WiFi Heatmap Monster");
 
-  setup_servo();
   setup_stepper();
   setup_wifi();
   
@@ -166,7 +189,7 @@ void setup() {
 }
 
 void loop() {
-  while(step_count <= step_count_max) {
+  while(hrzStepPos <= STEPS_A_REVOLUTION) {
     // get heatmap_pixel
     int heatmap_pixel = wifi_meanrssi();
     bool success_add = add_heatmap_pixel_map(heatmap_pixel);
@@ -178,14 +201,25 @@ void loop() {
     Serial.print("wifi mean rssi: ");
     Serial.println(heatmap_pixel);
     
-    // continue servo zigzag movement, if reach edge return true
-    bool reach_edge = servo_move();
-    delay(1000);
+    // continue vert_stepper zigzag movement, if reach edge return true
+    vertical_step(vrtStep);
 
-    if (reach_edge) {
-      // rotate horizontally by adding step to stepper
-      step();  
-      delay(1000);
+    // check if reached bottom or top, then change neg/pos accordingly
+    bool isVrtEdgeBottom = vrtStepPos >= 50;
+    bool isVrtEdgeTop = vrtStepPos <= 0;
+    bool isVrtEdge = isVrtEdgeBottom || isVrtEdgeTop; // edge is position 90 degree or 0 degree
+    delay(500);
+
+    if (isVrtEdge) {
+      // if reached edge, rotate horizontally
+      horizontal_step(hrzStep);
+
+      if (isVrtEdgeBottom) {
+        vrtStep = -1 * abs(vrtStep);
+      } else if (isVrtEdgeTop) {
+        vrtStep = 1 * abs(vrtStep);
+      }
+      delay(500);
     }
   }
 
